6a61728f957fdfe6a52fc9d396dd15a38414af70
Dr. Christian Kohlschütter <kohlschuetter@users.noreply.github.com>
2010-11-21 13:35:13 +0000

    Compare text blocks in lower-case, replaced Pattern matcher by hard-coded string compare (marginal speed-up) Added new terminating block "post a comment"
    see http://code.google.com/p/boilerpipe/issues/detail?id=12

--- src/main/de/l3s/boilerpipe/filters/english/TerminatingBlocksFinder.java
+++ src/main/de/l3s/boilerpipe/filters/english/TerminatingBlocksFinder.java
@@ -17,8 +17,6 @@
  */
 package de.l3s.boilerpipe.filters.english;
 
-import java.util.regex.Pattern;
-
 import de.l3s.boilerpipe.BoilerpipeFilter;
 import de.l3s.boilerpipe.BoilerpipeProcessingException;
 import de.l3s.boilerpipe.document.TextBlock;
@@ -26,54 +24,94 @@ import de.l3s.boilerpipe.document.TextDocument;
 import de.l3s.boilerpipe.labels.DefaultLabels;
 
 /**
- * Finds blocks which are potentially indicating the end of an article text and marks
- * them with {@link DefaultLabels#INDICATES_END_OF_TEXT}. This can be used in conjunction
- * with a downstream {@link IgnoreBlocksAfterContentFilter}. 
+ * Finds blocks which are potentially indicating the end of an article text and
+ * marks them with {@link DefaultLabels#INDICATES_END_OF_TEXT}. This can be used
+ * in conjunction with a downstream {@link IgnoreBlocksAfterContentFilter}.
  * 
  * @author Christian Kohlschütter
  * @see IgnoreBlocksAfterContentFilter
  */
 public class TerminatingBlocksFinder implements BoilerpipeFilter {
-    public static final TerminatingBlocksFinder INSTANCE = new TerminatingBlocksFinder();
+	public static final TerminatingBlocksFinder INSTANCE = new TerminatingBlocksFinder();
 
-    /**
-     * Returns the singleton instance for TerminatingBlocksFinder.
-     */
-    public static TerminatingBlocksFinder getInstance() {
-        return INSTANCE;
-    }
-    
-    private static final Pattern N_COMMENTS = Pattern.compile("(?msi)^[0-9]+ (Comments|users responded in)");
+	/**
+	 * Returns the singleton instance for TerminatingBlocksFinder.
+	 */
+	public static TerminatingBlocksFinder getInstance() {
+		return INSTANCE;
+	}
 
-    public boolean process(TextDocument doc)
-            throws BoilerpipeProcessingException {
-        boolean changes = false;
+	// public static long timeSpent = 0;
 
-        for (TextBlock tb : doc.getTextBlocks()) {
-            if (tb.getNumWords() < 20) {
-                final String text = tb.getText().trim();
-                if (text.startsWith("Comments")
-                        || N_COMMENTS.matcher(text).find()
-                        || text.contains("What you think...")
-                        || text.contains("add your comment")
-                        || text.contains("Add your comment")
-                        || text.contains("Add Your Comment")
-                        || text.contains("Add Comment")
-                        || text.contains("Reader views")
-                        || text.contains("Have your say")
-                        || text.contains("Have Your Say")
-                        || text.contains("Reader Comments")
-                        || text.equals("Thanks for your comments - this feedback is now closed")
-                        || text.startsWith("© Reuters")
-                        || text.startsWith("Please rate this")
-                ) {
-                    tb.addLabel(DefaultLabels.INDICATES_END_OF_TEXT);
-                    changes = true;
-                }
-            }
-        }
+	public boolean process(TextDocument doc)
+			throws BoilerpipeProcessingException {
+		boolean changes = false;
 
-        return changes;
-    }
+		// long t = System.currentTimeMillis();
+
+		for (TextBlock tb : doc.getTextBlocks()) {
+			final int numWords = tb.getNumWords();
+			if (numWords < 15) {
+				final String text = tb.getText().trim();
+				final int len = text.length();
+				if (len >= 8) {
+					final String textLC = text.toLowerCase();
+					if (textLC.startsWith("comments")
+							|| startsWithNumber(textLC, len, " comments",
+									" users responded in")
+							|| textLC.startsWith("© reuters")
+							|| textLC.startsWith("please rate this")
+							|| textLC.startsWith("post a comment")
+							|| textLC.contains("what you think...")
+							|| textLC.contains("add your comment")
+							|| textLC.contains("add comment")
+							|| textLC.contains("reader views")
+							|| textLC.contains("have your say")
+							|| textLC.contains("reader comments")
+							|| textLC
+									.equals("thanks for your comments - this feedback is now closed")) {
+						tb.addLabel(DefaultLabels.INDICATES_END_OF_TEXT);
+						changes = true;
+					}
+				}
+			}
+		}
+
+		// timeSpent += System.currentTimeMillis() - t;
+
+		return changes;
+	}
+
+	/**
+	 * Checks whether the given text t starts with a sequence of digits,
+	 * followed by one of the given strings.
+	 * 
+	 * @param t
+	 *            The text to examine
+	 * @param len
+	 *            The length of the text to examine
+	 * @param str
+	 *            Any strings that may follow the digits.
+	 * @return true if at least one combination matches
+	 */
+	private static boolean startsWithNumber(final String t, final int len,
+			final String... str) {
+		int j = 0;
+		while (j < len && isDigit(t.charAt(j))) {
+			j++;
+		}
+		if (j != 0) {
+			for (String s : str) {
+				if (t.startsWith(s, j)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	private static boolean isDigit(final char c) {
+		return c >= '0' && c <= '9';
+	}
 
 }
