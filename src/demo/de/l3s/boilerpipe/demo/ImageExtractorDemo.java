c3c16d94cbf19000fd39b14fb0508922ad8364de
Dr. Christian Kohlschütter <kohlschuetter@users.noreply.github.com>
2011-10-02 13:02:02 +0000

    Some API changes for the ImageExtractor.

--- src/demo/de/l3s/boilerpipe/demo/ImageExtractorDemo.java
+++ src/demo/de/l3s/boilerpipe/demo/ImageExtractorDemo.java
@@ -1,13 +1,20 @@
 package de.l3s.boilerpipe.demo;
 
 import java.net.URL;
+import java.util.Collections;
 import java.util.List;
 
 import de.l3s.boilerpipe.BoilerpipeExtractor;
+import de.l3s.boilerpipe.document.Image;
 import de.l3s.boilerpipe.extractors.CommonExtractors;
 import de.l3s.boilerpipe.sax.ImageExtractor;
 
-public class ImageExtractorDemo {
+/**
+ * Demonstrates how to use Boilerpipe to get the images within the main content.
+ * 
+ * @author Christian Kohlschütter
+ */
+public final class ImageExtractorDemo {
 	public static void main(String[] args) throws Exception {
 		URL url = new URL(
 				"http://www.spiegel.de/wissenschaft/natur/0,1518,789176,00.html");
@@ -20,9 +27,13 @@ public class ImageExtractorDemo {
 
 		final ImageExtractor ie = ImageExtractor.INSTANCE;
 		
-		List<String> imgUrls = ie.getEnclosedImages(url, extractor);
-		for(String u : imgUrls) {
-			System.out.println("* "+u);
+		List<Image> imgUrls = ie.process(url, extractor);
+		
+		// automatically sorts them by decreasing area, i.e. most probable true positives come first
+		Collections.sort(imgUrls);
+		
+		for(Image img : imgUrls) {
+			System.out.println("* "+img);
 		}
 
 	}
--- src/main/de/l3s/boilerpipe/sax/ImageExtractor.java
+++ src/main/de/l3s/boilerpipe/sax/ImageExtractor.java
@@ -19,20 +19,47 @@ import org.xml.sax.SAXException;
 
 import de.l3s.boilerpipe.BoilerpipeExtractor;
 import de.l3s.boilerpipe.BoilerpipeProcessingException;
+import de.l3s.boilerpipe.document.Image;
 import de.l3s.boilerpipe.document.TextBlock;
 import de.l3s.boilerpipe.document.TextDocument;
 
-public class ImageExtractor {
-	
+/**
+ * Extracts the images that are enclosed by extracted content. 
+ * 
+ * @author Christian Kohlschütter
+ */
+public final class ImageExtractor {
 	public static final ImageExtractor INSTANCE = new ImageExtractor();
 
-	public List<String> getEnclosedImages(final TextDocument doc,
+	/**
+	 * Processes the given {@link TextDocument} and the original HTML text (as a
+	 * String).
+	 * 
+	 * @param doc
+	 *            The processed {@link TextDocument}.
+	 * @param origHTML
+	 *            The original HTML document.
+	 * @return A List of enclosed {@link Image}s
+	 * @throws BoilerpipeProcessingException
+	 */
+	public List<Image> process(final TextDocument doc,
 			final String origHTML) throws BoilerpipeProcessingException {
-		return getEnclosedImages(doc, new InputSource(
+		return process(doc, new InputSource(
 				new StringReader(origHTML)));
 	}
 
-	public List<String> getEnclosedImages(final TextDocument doc,
+	/**
+	 * Processes the given {@link TextDocument} and the original HTML text (as an
+	 * {@link InputSource}).
+	 * 
+	 * @param doc
+	 *            The processed {@link TextDocument}.
+	 * @param origHTML
+	 *            The original HTML document.
+	 * @return A List of enclosed {@link Image}s
+	 * @throws BoilerpipeProcessingException
+	 */
+	public List<Image> process(final TextDocument doc,
 			final InputSource is) throws BoilerpipeProcessingException {
 		final Implementation implementation = new Implementation();
 		implementation.process(doc, is);
@@ -40,7 +67,18 @@ public class ImageExtractor {
 		return implementation.linksHighlight;
 	}
 	
-	public List<String> getEnclosedImages(final URL url, final BoilerpipeExtractor extractor)
+	/**
+	 * Fetches the given {@link URL} using {@link HTMLFetcher} and processes the
+	 * retrieved HTML using the specified {@link BoilerpipeExtractor}.
+	 * 
+	 * @param doc
+	 *            The processed {@link TextDocument}.
+	 * @param is
+	 *            The original HTML document.
+	 * @return A List of enclosed {@link Image}s
+	 * @throws BoilerpipeProcessingException
+	 */
+	public List<Image> process(final URL url, final BoilerpipeExtractor extractor)
 			throws IOException, BoilerpipeProcessingException, SAXException {
 		final HTMLDocument htmlDoc = HTMLFetcher.fetch(url);
 
@@ -50,14 +88,14 @@ public class ImageExtractor {
 
 		final InputSource is = htmlDoc.toInputSource();
 
-		return getEnclosedImages(doc, is);
+		return process(doc, is);
 	}
 	
 
 	private final class Implementation extends AbstractSAXParser implements
 			ContentHandler {
-		List<String> linksHighlight = new ArrayList<String>();
-		private List<String> linksBuffer = new ArrayList<String>();
+		List<Image> linksHighlight = new ArrayList<Image>();
+		private List<Image> linksBuffer = new ArrayList<Image>();
 
 		private int inIgnorableElement = 0;
 		private int characterElementIdx = 0;
@@ -125,7 +163,7 @@ public class ImageExtractor {
 					if(inHighlight && "IMG".equalsIgnoreCase(localName)) {
 						String src = atts.getValue("src");
 						if(src != null && src.length() > 0) {
-							linksBuffer.add(src);
+							linksBuffer.add(new Image(src, atts.getValue("width"), atts.getValue("height"), atts.getValue("alt")));
 						}
 					}
 				}
