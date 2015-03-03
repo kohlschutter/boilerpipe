f3fb3b49aac9b357f7c859825055b359e33931f3
burton <burton@spinn3r.com>
2015-03-02 16:54:29 -0800

    cleanup

--- boilerpipe-common/src/main/java/com/kohlschutter/boilerpipe/text/Text.java
+++ boilerpipe-common/src/main/java/com/kohlschutter/boilerpipe/text/Text.java
@@ -8,8 +8,7 @@ import java.util.regex.Pattern;
 public class Text {
 
     // TODO: probably migrate to ICU4J for this.
-    private static final Pattern PAT_VALID_WORD_CHARACTER = Pattern
-                                                              .compile("[\\p{L}\\p{Nd}\\p{Nl}\\p{No}]");
+    private static final Pattern PAT_VALID_WORD_CHARACTER = Pattern.compile("[\\p{L}\\p{Nd}\\p{Nl}\\p{No}]");
 
     public static boolean isWord(final String token) {
         return PAT_VALID_WORD_CHARACTER.matcher(token).find();
